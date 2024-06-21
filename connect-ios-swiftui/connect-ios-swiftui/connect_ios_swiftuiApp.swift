4b7a0e09cf47829c6e5e3565da43822f50be20ed
Jeffrey Yun <thisishaoyu@gmail.com>
2024-06-21 16:22:42 -0700

    Revert "Revert "feat: add swiftui example""

---
+++ connect-ios-swiftui/connect-ios-swiftui/connect_ios_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  connect_ios_swiftuiApp.swift
+//  connect-ios-swiftui
+//
+//  Created by Haoyu Yun on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct connect_ios_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
