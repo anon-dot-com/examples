028ebb46af1523b942775acdc2fb4b73be60eef0
Sam <sam@team.anon.com>
2024-07-10 22:43:20 -0700

    feat: first pass of AppClip

---
+++ connect-ios-swiftui/LinkClip/LinkClipApp.swift
@@ -0,0 +1,48 @@
+//
+//  LinkClipApp.swift
+//  LinkClip
+//
+//  Created by Sam Green on 7/10/24.
+//
+
+import SwiftUI
+import AnonKit
+import Foundation
+
+@main
+struct LinkClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .onContinueUserActivity("com.anon.dev.connect.FastLink", perform: { userActivity in
+                    if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+                        print("Launched via userActivity: \(userActivity.debugDescription)")
+                    }
+                })
+        }
+    }
+    
+    init() {
+        loadRocketSimConnect()
+    }
+    
+    private func loadRocketSimConnect() {
+        #if DEBUG
+        guard (Bundle(path: "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework")?.load() == true) else {
+            print("Failed to load linker framework")
+            return
+        }
+        print("RocketSim Connect successfully linked")
+        #endif
+    }
+    
+    private func loadTLSWorkaround() {
+//        AnonClient.session.delegate = TLSDelegate()
+//        -URLSession:didReceiveChallenge:completionHandler: delegate method. To customize HTTPS server trust evaluation, look for a challenge whose protection space has an authentication method of NSURLAuthenticationMethodServerTrust. For those challenges, resolve them as described below. For other challenges, the ones that you don't care about, call the completion handler block with the NSURLSessionAuthChallengePerformDefaultHandling disposition and a NULL credential.
+//
+//        When dealing with the NSURLAuthenticationMethodServerTrust authentication challenge, you can get the trust object from the challenge's protection space by calling the -serverTrust method. After using the trust object to do your own custom HTTPS server trust evaluation, you must resolve the challenge in one of two ways:
+//
+//        If you want to deny the connection, call the completion handler block with the NSURLSessionAuthChallengeCancelAuthenticationChallenge disposition and a NULL credential.
+//        If you want to allow the connection, create a credential from your trust object (using +[NSURLCredential credentialForTrust:]) and call the completion handler block with that credential and the NSURLSessionAuthChallengeUseCredential disposition.
+    }
+}
--- connect-ios-swiftui/connect-ios-swiftui/ContentView.swift
+++ connect-ios-swiftui/connect-ios-swiftui/ContentView.swift
@@ -44,28 +44,43 @@ struct AppRow: View {
 }
 
 struct ContentView: View {
+#if APPCLIP
+    @State private var isPresenting = true // State to manage SDK presentation
+#else
     @State private var isPresenting = false // State to manage SDK presentation
-    @State private var selectedApp = ""
+#endif
+    @State private var selectedApp = "linkedin"
 
     // Configuration for Anon SDK
     let anonConfig = AnonKit.Config(
         // Change to the appropriate environment as needed.
-        environment: .sandbox,
+        environment: .staging,
         // The uuid of your SdkClient /associated with your UserPool/
         // ie the one which returned   "auth": { "type": "userPool", "userPoolId": "..." }
-        clientId: "your-client-id-here",
-        // Your application user ID token, eg the JWT
-        // May be Anon-provided as the `example_app_user_id_token` from 1pass
-        appUserIdToken: "your-jwt-id-token-here"
+        clientId: "d3d694c2-dedd-4696-880c-c605a89afe64",
+                    // todo: grab from env.ANON_APP_USER_ID_TOKEN
+        appUserIdToken: "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFYQXotRy1xeFIxbmdnSzlHZ2wzNktGckpTQzB3TzUzVVdCbEd3NjVaYWcifQ.eyJlbWFpbCI6Iml0ZXN0LWFwcC11c2VyLTg3NDZAdGVhbS5hbm9uLmNvbSIsImlhdCI6MTcwODYzNzYyMSwiaXNzIjoidXJuOmV4YW1wbGU6aXNzdWVyIiwiYXVkIjoidXJuOmV4YW1wbGU6YXVkaWVuY2UiLCJleHAiOjE3MTEyMjk2MjEsInN1YiI6ImV4dGVybmFsLWlkLTczNDgifQ.Ptc0416ZzWSFrqiYvIbojrFjBDAV7d_ny4wQT-NjQINxEQ4tpxK3N26v85cHbMZ6gnsgSQF5k8WtjU__XkFc2VRWzbNi2XLHq51SZFDmsX0vkbFN5Y7PYQK5R51-nph9q_56kMdGwIfTbGhRg6LAb6ZCWYOHMwUolNrGcPvdLV6BxluoQY3iLMswsAaXLdcbvDvoy5yRXSwJIeEZRPXtmnCaoIAF-sKVH-1zk70oyWaUkf2c4jZaXiYKJ9qAwJwcbvrh7i4Zx6gzskmcYZX1OE6W32X3mfdoafjdI4K6JKweEDzZ0xhXyAHyS6Wd0lCqrRcb66I0sNFI4P5tdvvUvA"
     )
 
     var body: some View {
         NavigationView {
+#if APPCLIP
+            Text("App Clip!")
+                .font(.largeTitle)
+#else
             VStack(spacing: 32) {
                 AppButton(appRow: AppRow(
                     imageName: "fork.knife.circle.fill",
                     imageTint: .orange,
                     appName: "resy"
+                ), onTap: { newApp in
+                    selectedApp = newApp
+                })
+                
+                AppButton(appRow: AppRow(
+                    imageName: "table.furniture.fill",
+                    imageTint: .red,
+                    appName: "opentable"
                 ), onTap: { newApp in
                     selectedApp = newApp
                 })
@@ -102,6 +117,7 @@ struct ContentView: View {
                     selectedApp = newApp
                 }
             }
+#endif
         }
         .onChange(of: selectedApp, { oldValue, newValue in
             isPresenting.toggle()
